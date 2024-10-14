c77cc9693f7c5e540c34696611306b3a91fccf19
Randy Cullins <randycullins2018@gmail.com>
2024-10-14 11:10:40 -0600

    extracted Home class to new folder and imported it to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:coffee_card/home.dart';
+
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text("My Coffee Card"),
-            backgroundColor: Colors.brown[700],
-            centerTitle: true,
-          ),
-          body: const Home())));
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange,
-      padding: const EdgeInsets.all(20),
-      margin: const EdgeInsets.fromLTRB(10, 40, 0, 0),
-      child: const Text("Hello There!!",
-          style: TextStyle(
-              fontSize: 18,
-              letterSpacing: 4,
-              decoration: TextDecoration.underline,
-              fontStyle: FontStyle.italic)),
-    );
-  }
+  runApp(const MaterialApp(home: Home()));
 }
